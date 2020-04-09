f08beb448d596e93aa17451b8c4b2deacab0dc98
celiopleit <celiopereiraleite@gmail.com>
2020-04-08 21:31:26 -0300

    pp

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    title: 'Fluttefffr      Demo',
+    title: 'Fluttefffr      celio',
       theme: ThemeData(
         // This is the theme of your application.
         //
